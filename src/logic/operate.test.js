6ef5f5a1fcbb4c911756a1ef0a3046ca01a5772f
unorji kingsley <unorjikingsley@gmail.com>
2023-06-07 17:03:31 +0100

    created test using jest for logic/calculator

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,55 +1,84 @@
 import operate from './operate';
 
-describe('operate functions', () => {
-  it('adds', () => {
-    expect(operate(2, 1, '+')).toBe('3');
+describe('checking operators functionalities', () => {
+  const numberOne = '110';
+  const numberTwo = '40';
+  let operationSymbol;
+
+  test('addition operator', () => {
+    operationSymbol = '+';
+    expect(operate(numberOne, numberTwo, operationSymbol)).toEqual('150');
   });
 
-  it('adds', () => {
-    expect(operate(5, 5, '+')).toBe('10');
+  test('minus operator', () => {
+    operationSymbol = '-';
+    expect(operate(numberOne, numberTwo, operationSymbol)).toEqual('70');
   });
 
-  it('substracts', () => {
-    expect(operate(2, 1, '-')).toBe('1');
+  test('multiplication operator', () => {
+    operationSymbol = 'x';
+    expect(operate(numberOne, numberTwo, operationSymbol)).toEqual('4400');
   });
-
-  it('substracts', () => {
-    expect(operate(5, 5, '-')).toBe('0');
+  test('division operator', () => {
+    operationSymbol = '÷';
+    expect(operate(numberOne, numberTwo, operationSymbol)).toEqual('2.75');
   });
-
-  it('multiplies', () => {
-    expect(operate(2, 1, 'x')).toBe('2');
-  });
-
-  it('multiplies', () => {
-    expect(operate(5, 5, 'x')).toBe('25');
-  });
-
-  it('divides', () => {
-    expect(operate(2, 1, '÷')).toBe('2');
-  });
-
-  it('divides', () => {
-    expect(operate(5, 5, '÷')).toBe('1');
-  });
-
-  it('fails to divide by 0', () => {
-    expect(operate(2, 0, '÷')).toBe("Can't divide by 0.");
-  });
-
-  it('% finds modulus', () => {
-    expect(operate(2, 1, '%')).toBe('0');
-  });
-
-  it('% finds modulus', () => {
-    expect(operate(5, 5, '%')).toBe('0');
-  });
-
-  it('% fails to finds modulus by 0', () => {
-    expect(operate(5, 0, '%')).toBe("Can't find modulo as can't divide by 0.");
-  });
-
-  it('throws error for unknown operation', () => {
-    expect(() => operate(2, 1, '?')).toThrow("Unknown operation '?'");
+  test('modulo operator', () => {
+    operationSymbol = '%';
+    expect(operate(numberOne, numberTwo, operationSymbol)).toEqual('30');
   });
 });
+
+// describe('operate functions', () => {
+//   it('adds', () => {
+//     expect(operate(2, 1, '+')).toBe('3');
+//   });
+
+//   it('adds', () => {
+//     expect(operate(5, 5, '+')).toBe('10');
+//   });
+
+//   it('substracts', () => {
+//     expect(operate(2, 1, '-')).toBe('1');
+//   });
+
+//   it('substracts', () => {
+//     expect(operate(5, 5, '-')).toBe('0');
+//   });
+
+//   it('multiplies', () => {
+//     expect(operate(2, 1, 'x')).toBe('2');
+//   });
+
+//   it('multiplies', () => {
+//     expect(operate(5, 5, 'x')).toBe('25');
+//   });
+
+//   it('divides', () => {
+//     expect(operate(2, 1, '÷')).toBe('2');
+//   });
+
+//   it('divides', () => {
+//     expect(operate(5, 5, '÷')).toBe('1');
+//   });
+
+//   it('fails to divide by 0', () => {
+//     expect(operate(2, 0, '÷')).toBe("Can't divide by 0.");
+//   });
+
+//   it('% finds modulus', () => {
+//     expect(operate(2, 1, '%')).toBe('0');
+//   });
+
+//   it('% finds modulus', () => {
+//     expect(operate(5, 5, '%')).toBe('0');
+//   });
+
+//   it('% fails to finds modulus by 0', () => {
+//     expect(operate(5, 0, '%')).toBe("Can't find modulo as can't divide by 0.");
+//   });
+
+//   it('throws error for unknown operation', () => {
+//     expect(() => operate(2, 1, '?')).toThrow("Unknown operation '?'");
+//   });
+// });
